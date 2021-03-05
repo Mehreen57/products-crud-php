86e7b085c2230bed510796d885e8a06e0aca3b4c
MEHREEN <mehreenbhat77@gmail.com>
2021-03-05 15:50:50 +0530

    added table

--- index.php
+++ index.php
@@ -12,7 +12,42 @@
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
-
+    <section class = "wrapper">
+        <div class = "container">
+          <h1>Products Crud</h1>
+          <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </section>
+    
     
     
     <!-- load scripts -->
